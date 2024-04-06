5665365b57d018c116d383dffd98fcbe20ece5b3
Eduardo Bustamante <betobustamante.ef@gmail.com>
2024-04-06 06:57:35 -0500

    Initial commit

---
+++ test/demo.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const Demo = require('../lib/demo-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/demo-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new Demo.DemoStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
